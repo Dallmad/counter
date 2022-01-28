708a9e557417550a954b189def544443a8d038cd
Dzmitry <maddal.privod@gmail.com>
2022-01-28 22:50:17 +0300

    universal button

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import s from './Buttons.module.css'
+
+type ButtonType = {
+    name: string,
+    callBack: () => void
+    disable: boolean
+}
+
+export const Button = (props: ButtonType) => {
+    return (
+        <button
+            className={props.disable ? s.noActive : s.button}
+            disabled={props.disable}
+            onClick={() => props.callBack()}>
+            {props.name}
+        </button>
+    )
+}
