f762ac4f7d948c3cf01eec6135ddbd49f8b3a4bf
Dzmitry <maddal.privod@gmail.com>
2022-01-28 22:50:39 +0300

    scoreBoard

---
+++ src/components/scoreboard/scoreBoard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from './ScoreBoard.module.css'
+
+type ScoreBoardType = {
+    counter: number
+    scoreMax:number
+}
+
+export const ScoreBoard = (props: ScoreBoardType) => {
+    return (
+        <div className={props.counter===props.scoreMax ? s.scoreMax : ''}>
+            {props.counter}
+        </div>
+    )
+}
