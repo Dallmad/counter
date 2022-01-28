f0d86853d8417287e766568069cdd7ca883824f9
Dzmitry <maddal.privod@gmail.com>
2022-01-28 22:50:52 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button} from "./components/Buttons/Button";
+import {ScoreBoard} from './components/scoreboard/scoreBoard'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [counter, setCounter] = useState(0)
+    const scoreMax = 5;
+    const scoreMin = 0;
+    const clickIncrement = () => {
+        setCounter(counter < scoreMax ? ++counter : counter = scoreMax)
+    }
+    const clickReset = () => {
+        setCounter(counter = scoreMin)
+    }
+
+    return (
+        <div>
+            <div className='app'>
+                <div>
+                    <ScoreBoard
+                        counter={counter}
+                        scoreMax={scoreMax}
+                    />
+                </div>
+                <div className='button'>
+                    <Button
+                        disable={counter === scoreMax}
+                        name='Inc'
+                        callBack={clickIncrement}
+                    />
+                    <Button
+                        disable={counter === scoreMin}
+                        name='Reset'
+                        callBack={clickReset}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
